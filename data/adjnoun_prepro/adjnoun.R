4f3bc1692b313ec4404efc70c8a7a9453112dca6
Allen Kei <allen29@ucla.edu>
2025-01-22 17:53:35 -0800

    Add files via upload

---
+++ data/adjnoun_prepro/adjnoun.R
@@ -0,0 +1,16 @@
+library(networkdata)
+library(igraph)
+library(reticulate)
+np <- import("numpy")
+data(adjnoun)
+
+
+adj <- as.matrix(as_adjacency_matrix(adjnoun))
+node_labels <- V(adjnoun)$name
+node_classes <- V(adjnoun)$value 
+
+adj <- np$array(adj)
+node_labels <- np$array(node_labels)
+node_classes <- np$array(node_classes)
+
+np$savez("adjnoun_raw.npz", adj=adj, labels=node_labels, classes=node_classes)
---
+++ data/adjnoun_prepro/gen_word_vec.py
@@ -0,0 +1,15 @@
+import gensim.downloader as api
+import numpy as np
+
+model = api.load("glove-wiki-gigaword-100")
+data = np.load('data/adjnoun_data.npz', allow_pickle=True) # from R
+
+adj_matrix = data['adj'] # the adjacency matrix
+y_data = data['labels'] # the actual word
+y_classes = data['classes'] # 0 is adj, 1 is noun
+
+adjnoun_vectors = [model[word] if word in model else np.zeros(100) for word in y_data]
+adjnoun_vectors = np.array(adjnoun_vectors) # word embeddings
+
+np.savez('data/adjnoun_vec.npz', adj=adj_matrix, labels=y_data, classes=y_classes, word_vec = adjnoun_vectors)
+
